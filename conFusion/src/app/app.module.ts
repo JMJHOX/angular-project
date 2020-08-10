683cb6571711bbff08b8a6be3a592e92f68dbda6
jmjhox <josemiguelaparicio507@gmail.com>
2020-08-10 17:19:47 -0500

    Minor changes for assigment 3

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -22,6 +22,11 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -40,12 +45,16 @@ import {MatDialogModule} from '@angular/material/dialog';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
+    FormsModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [
     DishService,

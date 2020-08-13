bef7164a58fdff0921544be189a7d3274350724b
jmjhox <josemiguelaparicio507@gmail.com>
2020-08-13 10:43:16 -0500

    angular observables rx

--- conFusion/src/app/services/leader.service.ts
+++ conFusion/src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import {Observable,of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +11,25 @@ export class LeaderService {
 
   constructor() { }
 
-getLeaders():Promise<Leader[]> {
- return new Promise(resolve=>{
-    //simulate server latency with 2 second delay
+getLeaders():Observable<Leader[]> {
+  return of(LEADERS).pipe(delay(2000));
+ /*return new Promise(resolve=>{
     setTimeout(()=>resolve( LEADERS),2000);
-    });
+    });*/
 }
 
-getLeader(id: string): Promise<Leader> {
-  return new Promise(resolve=>{
-    //simulate server latency with 2 second delay
+getLeader(id: string): Observable<Leader> {
+  return of(LEADERS.filter((lead) => (lead.id === id))[0]).pipe(delay(2000));
+  /*return new Promise(resolve=>{
     setTimeout(()=>resolve( LEADERS.filter((lead) => (lead.id === id))[0]),2000);
-    });
+    });*/
 }
 
-getFeaturedLeader(): Promise<Leader> {
-  return new Promise(resolve=>{
-    //simulate server latency with 2 second delay
+getFeaturedLeader(): Observable<Leader> {
+  return of(LEADERS.filter((lead) => lead.featured)[0]).pipe(delay(2000));
+  /*return new Promise(resolve=>{
     setTimeout(()=>resolve( LEADERS.filter((lead) => lead.featured)[0]),2000);
-    });
+    });*/
 }
 
 }
